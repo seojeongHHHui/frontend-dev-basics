d90e1c83dd673b5a890345e44d9dbc7441a5b95a
seojeongHHHui <sjhui0523@gmail.com>
2024-07-08 15:20:59 +0900

    ajax #2

--- ch08/src/main/java/ch08/controller/test/ApiTestController.java
+++ ch08/src/main/java/ch08/controller/test/ApiTestController.java
@@ -6,10 +6,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test")
 public class ApiTestController {
-	
 	@RequestMapping("/text")
 	public String text() {
 		return "text";
+	}
+	
+	@RequestMapping("/html")
+	public String html() {
+		return "html";
+	}
+
+	@RequestMapping("/json1")
+	public String json1() {
+		return "json1";
+	}
+
+	@RequestMapping("/json2")
+	public String json2() {
+		return "json2";
 	}
 	
 }
